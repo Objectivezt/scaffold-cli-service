0eb5bef1df2ee07ebd5f6407ce10cc210faaf64b
Objectivezt <Objectivezt@gmail.com>
2020-08-08 16:24:51 +0800

    feat: bin

--- bin/dev.js
+++
@@ -1,8 +0,0 @@
-const Service = require('./service');
-const service = new Service();
-const { envFn } = require('./scaffold-cli-service');
-
-envFn();
-service.run('dev').catch(err => {
-  console.log(err);
-});
--- bin/scaffold-cli-service.js
+++ bin/scaffold-cli-service.js
@@ -1,9 +1,12 @@
+#!/usr/bin/env node
+
 const semver = require('semver');
 const path = require('path');
 const fs = require('fs');
 const chalk = require('chalk');
-const { PROJECT_PATH } = require('../config/getPathConfig');
+const { PROJECT_PATH } = require('../lib/config/getPathConfig');
 
+// 设置环境变量
 function setEnv(env) {
   if (fs.existsSync(env)) {
     require('dotenv-expand')(
@@ -15,8 +18,21 @@ function setEnv(env) {
 }
 
 const rawArgv = process.argv.slice(2);
+const commands = ['dev', 'build', 'analyzer'];
 
-console.log(rawArgv);
-exports.envFn = () => {
-  setEnv(`${PROJECT_PATH}/.env.${rawArgv[1] || rawArgv[0]}`);
-};
+//  无参数
+if (commands.indexOf(rawArgv[0]) < 0) {
+  console.log(`${chalk.green('运行')}  ${chalk.yellow('npm run dev')} 开发环境`);
+  console.log(`${chalk.green('运行')}  ${chalk.yellow('npm run build')} 项目打包`);
+  console.log(`${chalk.green('运行')}  ${chalk.yellow('npm run analyzer')} 包分析`);
+  process.exit(1);
+}
+
+setEnv(`${PROJECT_PATH}/.env.${rawArgv[1] || rawArgv[0]}`);
+
+const Service = require('../lib/service');
+const service = new Service();
+service.run(rawArgv[0]).catch(err => {
+  console.log(chalk.red(err));
+  process.exit(1);
+});
